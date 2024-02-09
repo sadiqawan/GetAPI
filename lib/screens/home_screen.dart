bc9ff588cc8e95cbaac5043ef3f77a6479b71520
sadiqawan <awansadiq09@gmail.com>
2024-02-09 23:25:16 +0500

    Api hit successfully...

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,24 +13,65 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Future<List<ProductModel>> getFromApi() async {
-
     List<ProductModel> products = [];
     String url = 'https://jsonplaceholder.typicode.com/photos';
-      http.Response response  =  await http.get(Uri.parse(url));
-      if(response.statusCode == 200){
-        var jsonResponse = jsonDecode(response.body);
-        for(var jsonProduct in jsonResponse){
-          ProductModel productModel = ProductModel.fromJson(jsonProduct);
-          products.add(productModel);
-        }
-        return products;
-      }else{
-        throw Exception('Something wrong');
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+      for (var jsonProduct in jsonResponse) {
+        ProductModel productModel = ProductModel.fromJson(jsonProduct);
+        products.add(productModel);
       }
+      return products;
+    } else {
+      throw Exception('Something wrong');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('Api Call')),
+        ),
+        body: FutureBuilder(
+          future: getFromApi(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<ProductModel> product = snapshot.data!;
+              return GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 1,
+                ),
+                itemCount: product.length,
+                itemBuilder: (context, int index) {
+                  ProductModel productModel = product[index];
+
+                  return SizedBox(
+
+                    child: Column(
+                      children: [
+                        Image.network('${productModel.thumbnailUrl}'),
+                        Text('Album : ${productModel.albumId}'),
+                        Text('PostId : ${productModel.id}'),
+                        Text('Album : ${productModel.title}'),
+                      ],
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return const Center(child: Text('Something went wrong'));
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.green,
+                ),
+              );
+            }
+          },
+        ),
+
+    );
   }
 }
